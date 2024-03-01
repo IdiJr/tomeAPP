4abf7f665bf73b0de5357dd8a8e10f28c5e6112d
emmanueladdo <asememmanueladd@gmail.com>
2024-03-01 04:37:01 +0000

    database sql code

--- tomeApp_sql_query.sql
+++ tomeApp_sql_query.sql
@@ -1,9 +1,9 @@
--- This creates the glosapp schema
+-- This creates the tomeApp schema
 
-CREATE SCHEMA IF NOT EXISTS `glosapp`;
+CREATE SCHEMA IF NOT EXISTS `tomeapp`;
 
 -- creates the product table and colums
-CREATE TABLE `glosapp`.`products` (
+CREATE TABLE `tomeapp`.`products` (
   `product_id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(100) NOT NULL,
   `uom_id` INT NOT NULL,
@@ -12,29 +12,29 @@ CREATE TABLE `glosapp`.`products` (
 );
 
 -- creates unit of messure  table 
-CREATE TABLE `glosapp`.`uom` (
+CREATE TABLE `tomeapp`.`uom` (
   `uom_id` INT NOT NULL AUTO_INCREMENT,
   `uom_name` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`uom_id`)
 );
 
 -- creates the place holder uom 
-INSERT INTO `glosapp`.`uom` (`uom_id`, `uom_name`) VALUES ('1', 'each ');
-INSERT INTO `glosapp`.`uom` (`uom_id`, `uom_name`) VALUES ('2', 'kg');
+INSERT INTO `tomeapp`.`uom` (`uom_id`, `uom_name`) VALUES ('1', 'each ');
+INSERT INTO `tomeapp`.`uom` (`uom_id`, `uom_name`) VALUES ('2', 'kg');
 
 -- creates a foreign key for unit of measurement in products table
-ALTER TABLE `glosapp`.`products`
+ALTER TABLE `tomeapp`.`products`
 ADD INDEX `fk_uom_id_idx` (`uom_id` ASC) VISIBLE;
 
-ALTER TABLE `glosapp`.`products`
+ALTER TABLE `tomeapp`.`products`
 ADD CONSTRAINT `fk_uom_id`
   FOREIGN KEY (`uom_id`)
-  REFERENCES `glosapp`.`uom` (`uom_id`)
+  REFERENCES `tomeapp`.`uom` (`uom_id`)
   ON DELETE NO ACTION
   ON UPDATE RESTRICT;
 
 -- creates orders table
-CREATE TABLE `glosapp`.`orders` (
+CREATE TABLE `tomeapp`.`orders` (
   `order_id` INT NOT NULL AUTO_INCREMENT,
   `customer_name` VARCHAR(225) NOT NULL,
   `total` DOUBLE NOT NULL,
@@ -43,7 +43,7 @@ CREATE TABLE `glosapp`.`orders` (
 );
 
 -- creates details for orders
-CREATE TABLE `glosapp`.`order_details` (
+CREATE TABLE `tomeapp`.`order_details` (
   `order_id` INT NOT NULL,
   `product_id` INT NOT NULL,
   `quantity` DOUBLE NOT NULL,
@@ -51,17 +51,17 @@ CREATE TABLE `glosapp`.`order_details` (
   INDEX `fk_product_id_idx` (`product_id` ASC) VISIBLE,
   CONSTRAINT `fk_order_id`
     FOREIGN KEY (`order_id`)
-    REFERENCES `glosapp`.`orders` (`order_id`)
+    REFERENCES `tomeapp`.`orders` (`order_id`)
     ON DELETE NO ACTION
     ON UPDATE RESTRICT,
   CONSTRAINT `fk_product_id`
     FOREIGN KEY (`product_id`)
-    REFERENCES `glosapp`.`products` (`product_id`)
+    REFERENCES `tomeapp`.`products` (`product_id`)
     ON DELETE NO ACTION
     ON UPDATE RESTRICT
 );
 
--- creates user called gstore and grants all privileges of glosapp to it
+-- creates user called gstore and grants all privileges of tomeapp to it
 CREATE USER 'gstore'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
-GRANT ALL PRIVILEGES ON glosapp.* TO 'gstore'@'localhost';
+GRANT ALL PRIVILEGES ON tomeapp.* TO 'gstore'@'localhost';
 FLUSH PRIVILEGES;
