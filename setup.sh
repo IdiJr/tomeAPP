0b7cff2550824252ab9d10a464c7e40ba5166eeb
IdiJr <idialijnr@gmail.com>
2024-03-05 17:20:25 +0000

    setup for server

--- setup.sh
+++ setup.sh
@@ -8,3 +8,6 @@ sudo apt-get install -y python3 mysql-server
 
 # Install Python packages
 sudo pip install mysql-connector-python Flask SQLAlchemy
+
+# Setup database to be populated
+cat tomeAPP_sql_query.sql | mysql
